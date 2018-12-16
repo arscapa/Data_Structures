f83d6cca90bac33af3897b7a08558dc750723d86
Tony Scapardine <arscapa@gmail.com>
2018-12-16 16:08:43 -0600

    Updated code to prepend values to list

--- Linked_list.cpp
+++ Linked_list.cpp
@@ -8,7 +8,7 @@ struct Node
 		Node* next;
 	};
 	
-void append(int val, Node* ptr)
+void append(int val, Node* & ptr)
 // Add item to end of list
 	{
 		// Create new node to add to list
@@ -16,14 +16,20 @@ void append(int val, Node* ptr)
 		(*item).value = val;
 		(*item).next = NULL;
 		
-		// Traverse through list until reach last item
-		Node* lastItem = ptr;
-		while(lastItem->next != NULL){
-			lastItem = lastItem->next;
+		if(ptr != NULL) 
+		{
+			// Traverse through list until reach last item
+			Node* lastItem = ptr;
+			while(lastItem->next != NULL){
+				lastItem = lastItem->next;
+			}
+			// Link new node to current last item in list
+			(*lastItem).next = item;
 		}
-		
-		// Link new node to current last item in list
-		(*lastItem).next = item; 
+		else
+		{
+			ptr = item;
+		}
 	};
 	
 	
@@ -40,7 +46,7 @@ void prepend(int val, Node* & ptr)
 
 	};
 	
-void printList(Node* ptr)
+void printList(const Node* ptr)
 	{
 		std::cout << (*ptr).value << std::endl;
 		if (ptr->next != NULL){
@@ -53,12 +59,6 @@ void printList(Node* ptr)
 int main()
 {
 	Node* listPtr = NULL;
-	Node* root = new Node;
-	
-	(*root).value = 2;			// could also set values by root->value = 2; 
-	(*root).next = NULL;		// could also set values by root->next = NULL;
-	
-	listPtr = root;				// set ptr equal to address of first node
 	
 	append(6,listPtr);
 	append(8,listPtr);
